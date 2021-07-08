a2e8535531926c0ce423ba3e9a34ebcbb3ea0150
naijie2108 <yeatnaijie@gmail.com>
2021-07-08 23:45:27 +0800

    revert

--- routes/payments.js
+++ routes/payments.js
@@ -9,18 +9,13 @@ router.get("/*", (req, res) => {
 router.post("/create", async (req, res) => {
   try {
     const { amount, shipping } = req.body;
-    if (shipping) {
-      const paymentIntent = await stripe.paymentIntents.create({
-        shipping,
-        amount,
-        currency: "sgd",
-      });
-    } else {
-      const paymentIntent = await stripe.paymentIntents.create({
-        amount,
-        currency: "sgd",
-      });
-    }
+
+    const paymentIntent = await stripe.paymentIntents.create({
+      shipping,
+      amount,
+      currency: "sgd",
+    });
+
     res.status(200).send(paymentIntent.client_secret);
   } catch (err) {
     res.status(500).json({
